d329c055b2bc22ff2678fefdcafd56ecf46f6e24
krishna-padaliya <140835816+krishna-padaliya@users.noreply.github.com>
2024-05-13 23:46:55 +0530

    task

---
+++ project 1/javascript.js
@@ -0,0 +1,34 @@
+// 1. What Is JavaScript ?
+// => JavaScript is a scripting language that enables you to create dynamically 
+//    updating content, control multimedia, animate images, and pretty much everything else. 
+
+// 2. Father Of JavaScript ?
+// => Brendan Eich.
+
+// => JavaScript was invented by Brendan Eich in 1995.
+// It was developed for Netscape 2, and became the ECMA-262 standard in 1997.
+// After Netscape handed JavaScript over to ECMA,
+// the Mozilla foundation continued to develop JavaScript for the Firefox browser.
+
+
+
+// 3. what an interpreter is and how it differs from a compiler. Provide examples of programming languages that use interpreters and those that use compilers.
+// ==> * diffrent Between interpreter and compiler :
+//         => interpreter Translates program one statement at a time.
+
+//         => compiler Scans the entire program and translates it as a whole into machine code.
+
+//         => Interpreters usually take less amount of time to analyze the source code. However, the overall execution time is comparatively slower than compilers.
+
+//         => Compilers usually take a large amount of time to analyze the source code. However, the overall execution time is comparatively faster than interpreters.
+
+        //    => In Interpreters Errors are displayed in every single line.
+
+        //    => In Compiler Errors are displayed in Compiler after Compiling together at the current time.
+
+
+// ==> * examples of programming languages that use interpreters and those that use compilers :
+
+//         => interpreter use Programming languages like JavaScript, Python, Ruby use interpreters.
+
+//         => compiler use Programming languages like C, C++, Java use compilers.
